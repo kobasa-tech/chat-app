fe21fe3dc1806d3dc6693d812f589667018308e1
kobasa-tech <warped.trump@gmail.com>
2021-06-23 23:29:43 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   # 既にルーティングにあるdevise/passwords#editとdevise/password#updateはpassword用。
   # users#editとusers#updateはないので作る必要がある。
