c59e9d659f53d331bbc5484076e6244e77357e62
kobasa-tech <warped.trump@gmail.com>
2021-06-29 01:18:51 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   # 既にルーティングにあるdevise/passwords#editとdevise/password#updateはpassword用。
   # users#editとusers#updateはないので作る必要がある。
-  resources :rooms, only: [:new, :create] do 
+  resources :rooms, only: [:new, :create, :destroy] do 
     resources :messages, onky: [:index, :create]#チャットはあるルームの中でしか書けないため、ネストを利用する。
   end
 end
