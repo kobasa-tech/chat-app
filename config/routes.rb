6e7bf7b31fa39ade01ff234af3002fbaaffc3f13
kobasa-tech <warped.trump@gmail.com>
2021-06-28 23:37:56 +0900

    メッセージの送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
   # 既にルーティングにあるdevise/passwords#editとdevise/password#updateはpassword用。
   # users#editとusers#updateはないので作る必要がある。
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do 
+    resources :messages, onky: [:index, :create]#チャットはあるルームの中でしか書けないため、ネストを利用する。
+  end
 end
